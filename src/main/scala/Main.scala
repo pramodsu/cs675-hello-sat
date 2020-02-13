fadc922b5460ee7fabc5d1746e607731a45dc7eb
Pramod Subramanyan <pramod.subramanyan@gmail.com>
2020-02-13 13:21:40 +0530

    Puzzle encoding.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,14 +10,32 @@ object Main {
     val an = ~a
     val bn = ~b
 
+    println("a = " + a.toString())
+    println("an = " + an.toString())
+
     val S = new Solver()
     S.addClause(a, b)
     S.addClause(~a, ~b)
     // Solve
+    println(S.solve(List(a)))
+    // Extract model.
+    var va = S.modelValue(a)
+    var vb = S.modelValue(b)
+    println("model: [a -> %s, b -> %s]".format(va.toString(), vb.toString()))
+
+    var la = if (va) ~a else a
+    var lb = if (vb) ~b else b
+    S.addClause(la, lb)
+
     println(S.solve())
     // Extract model.
-    val va = S.modelValue(a)
-    val vb = S.modelValue(b)
+    va = S.modelValue(a)
+    vb = S.modelValue(b)
     println("model: [a -> %s, b -> %s]".format(va.toString(), vb.toString()))
+
+    la = if (va) ~a else a
+    lb = if (vb) ~b else b
+    S.addClause(la, lb)
+
   }
 }
